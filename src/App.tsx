f4b2e5c7667fdb0e1792d64d18c0b8a2ba16ebea
DyudinDaniil <dyudindaniil2002@gmail.com>
2024-06-11 12:39:28 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import styles from './App.module.sass';
 
+import { Routes, Route, Link } from 'react-router-dom';
+
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import { Form } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <main className={styles.main}>
       <Navbar />
-      <Home />
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+
       <Footer />
     </main>
   )
